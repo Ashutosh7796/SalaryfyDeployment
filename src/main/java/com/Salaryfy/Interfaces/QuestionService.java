e40cc6cfad2fa4e8ebeb9265e5ece79dad1f382b
Ashutosh7796 <132451174+Ashutosh7796@users.noreply.github.com>
2023-10-09 15:38:07 +0530

    Made necessary changes required for deployment

--- src/main/java/com/Salaryfy/Interfaces/QuestionService.java
+++
@@ -1,9 +0,0 @@
-package com.Salaryfy.Interfaces;
-
-import com.Salaryfy.Dto.QuestionDTO;
-
-public interface QuestionService {
-    void postQuestions(QuestionDTO questionDTO);
-
-    void updateQuestion(QuestionDTO questionDTO);
-}
